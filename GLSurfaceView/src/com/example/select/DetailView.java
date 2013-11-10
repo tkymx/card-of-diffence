f7c8894de4805a7abf217dfa88ca26cba8169c08
kawapaso <KawazuHirotaka@kawadzuyuki-no-MacBook-Pro.local>
2013-11-10 18:38:37 +0900

    DeckSelectクラス、carddata_layout.xml、CardDataクラス作成
    ぐへへ

---
+++ GLSurfaceView/src/com/example/select/DetailView.java
@@ -0,0 +1,17 @@
+package com.example.select;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class DetailView extends View {
+
+	public DetailView(Context cn) {
+		super(cn);
+		
+		
+	}
+	public void onDraw(Canvas cs){
+		
+	}
+}
